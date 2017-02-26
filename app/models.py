772f40954bfffc17bce2e5f03554728354876f01
Luke Harrold <Luke Harrold>
2017-02-26 18:18:45 -0500

    added updated models for the non-existent database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from app import db
 
 # Models will go here once written
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    song_title = db.Column(db.String(55))
+    url = db.Column(db.String(55))
+    moods = db.relationship('Mood', backref='song', lazy='dynamic')
+    mood_id = db.Column(db.Integer, db.foreign_key('mood.id'))
+
+class Mood(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Columns(db.String(55))
+    
