dbe6e67683bb8ae724e9f72ce7e6794193b175d3
Jarryd Lee <jarrydrlee@gmail.com>
2017-02-27 11:28:22 -0500

    Fix typos

--- app/models.py
+++ app/models.py
@@ -7,13 +7,13 @@ class Song(db.Model):
     song_title = db.Column(db.String(55))
     url = db.Column(db.String(55))
     moods = db.relationship('Mood', backref='song', lazy='dynamic')
-    mood_id = db.Column(db.Integer, db.foreign_key('mood.id'))
+    mood_id = db.Column(db.Integer, db.ForeignKey('mood.id'))
 
 class Mood(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Columns(db.String(55))
-    
-class Environment(bd.Model):
+    name = db.Column(db.String(55))
+
+class Environment(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	gyro_x = db.Column(db.Float)
 	gyro_y = db.Column(db.Float)
@@ -28,5 +28,4 @@ class Environment(bd.Model):
 	accel_z = db.Column(db.Float)
 	air_pressure = db.Column(db.Float)
 	light = db.Column(db.Float)
-	
-    
+
