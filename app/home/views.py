d39a71bba7c04a01cba0ad8911bb80b86286e139
Derick Yung <Derick Yung>
2017-03-08 03:26:31 -0500

    Add Song modal skeleton

--- app/home/views.py
+++ app/home/views.py
@@ -9,3 +9,11 @@ def homepage():
     Render the homepage template on the / route
     """
     return render_template('home/index.html', title="Welcome")
+
+@home.route('/addSong')
+def addsong():
+    """
+    Render the add song template on the /addSong route
+    """
+    return render_template('home/addSong.html', title="Add Song")
+    
