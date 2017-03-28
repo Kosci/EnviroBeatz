d5c29e038b86c0b7904934b5875677b1fa6afc89
Luke Harrold <lvh3851@rit.edu>
2017-03-28 17:30:58 -0400

    tweaked gyroscop shaking functionality

--- app/home/gyro.py
+++ app/home/gyro.py
@@ -1,3 +1,17 @@
+from app.models
+from app.home import data
+
 def skip_song():
 	gyros = data.getGyroData()
 	
+	is_shaking = False
+	keys = gyros.keys()
+	for key in keys():
+		pos_orientation = True
+		for coord in gyros[key]:
+			if coord<0 and pos_orientation:
+				pos_orientation = False
+				is_shaking = True
+			elif coord>0 and !pos_orientation:
+				pos_orientation = True
+				is_shaking = True
